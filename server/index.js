afb3fa4f8576102949c2660718cb50848903d406
sandymon <rodriguezsandy0306@gmail.com>
2024-05-24 19:38:50 -0400

    added index.js and connection response

--- server/index.js
+++ server/index.js
@@ -2,4 +2,12 @@ import express from "express"
 
 const app = express()
 
-app.use('/')
+app.get('/' ,(res,req) => {
+
+    req.json("Server is operational")
+})
+
+app.listen(3000,() =>{
+    console.log("Server is operational");
+
+} )
