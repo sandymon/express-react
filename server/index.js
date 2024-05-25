d5ffcfcec057f143bc1c82de0b591b7bb0ff15fe
sandymon <rodriguezsandy0306@gmail.com>
2024-05-24 20:34:13 -0400

    added get / and /test get request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
 import express from "express"
+import cors from "cors"
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express()
 
-app.get('/test' ,(res,req) => {
+app.use('/server', express.static(path.join(__dirname)));
 
-    req.json("Server is operational")
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, '..', 'express-react', 'dist')));
+
+
+app.get('/' ,(req,res) => {
+    
+    res.sendFile(express.static(path.join(__dirname, '..', 'express-react', 'dist', 'index.html')));
+})
+
+app.get('/test' ,(req,res) => {
+    
+    res.sendFile(path.join(__dirname, 'Robbie_render.png'));
 })
 
 app.listen(3000,() =>{
